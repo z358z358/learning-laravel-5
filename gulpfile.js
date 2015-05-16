c97ad6a42b7d9d61550ede1980dbf0c6cf9ccc08
Евгений Глотов <kivagant@gmail.com>
2015-05-17 01:36:59 +0300

    bugfix: setting resources folders for run gulp with actual version of elixir

--- gulpfile.js
+++ gulpfile.js
@@ -16,16 +16,16 @@ elixir(function(mix) {
     mix.sass('app.scss', 'resources/css');
 
     mix.styles([
-    	'libs/bootstrap.min.css',
-    	'app.css',
-    	'libs/select2.min.css'
-    ]);
+        'libs/bootstrap.min.css',
+        'app.css',
+        'libs/select2.min.css'
+    ], 'public/css/all.css', 'resources/css');
 
     mix.scripts([
-    	'libs/jquery.js',
-    	'libs/bootstrap.min.js',
-    	'libs/select2.min.js'
-    ]);
+        'libs/jquery.js',
+        'libs/bootstrap.min.js',
+        'libs/select2.min.js'
+    ], 'public/js/all.js', 'resources/js');
 
     mix.version(['css/all.css', 'js/all.js']);
 
